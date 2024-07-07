82b677fcd5afa4acb2329092f9c923a50d2fa822
liuzhiyong1982 <73316431+liuzhiyong1982@users.noreply.github.com>
2024-07-07 17:44:18 -0600

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.111.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
